088041a47bdc79abe49866f7706642b49f7c9b75
VladimirKalinin <vovakalinin1997@gmail.com>
2015-12-15 02:14:58 +0300

    Prinimay testi bystro

--- project/Lists/test/test_CNode.cpp
+++ project/Lists/test/test_CNode.cpp
@@ -2,7 +2,10 @@
 
 #include "C:/Ilya/Lists/project/Lists/gtest/gtest.h"
 #include <iostream>
-TEST(CNode, can_vova_do_nothing)
+
+
+
+TEST(CNode, can_recognize_palindrom_with_odd_lenth)
 {
   struct CNode* t;
   struct CNode* start; 
@@ -34,9 +37,161 @@ TEST(CNode, can_vova_do_nothing)
     std::cout << t -> val << ' ';
 	t=t->next;
   }	
+  printf("\n");
+}
 
+TEST(CNode, can_recognize_palindrom_with_even_lenth)
+{
+  struct CNode* t;
+  struct CNode* start; 
+  start = new struct CNode();
+	start->val = 0;
+	start->next = 0;
+	std::cout << start -> val << ' ';
+    t=start;
+  for(int i = 1; i < 11; i++) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+  }
 
+	for(int i = 10; i > -1; i--) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+     
+  }
+  std:: cout << std:: endl;
+  t=start;
+  EXPECT_EQ(1, palindrom(&start));
+  while(t!=0) {
+    std::cout << t -> val << ' ';
+	t=t->next;
+  }	
+  printf("\n");
+}
 
+TEST(Cnode, can_recognize_not_palindrom_by_value)
+{
+  struct CNode* t;
+  struct CNode* start; 
+  start = new struct CNode();
+	start->val = 0;
+	start->next = 0;
+	std::cout << start -> val << ' ';
+    t=start;
+  for(int i = 1; i < 10; i++) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+  }
 
- 
+	for(int i = 10; i > -1; i--) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+     
+  }
+	t=start;
+	for(int i = 1; i < 7; i++)
+		t=t->next;
+	t->val=72;
+
+  std:: cout << std:: endl;
+  t=start;
+  EXPECT_NE(1, palindrom(&start));
+  while(t!=0) {
+    std::cout << t -> val << ' ';
+	t=t->next;
+  }	
+    printf("\n");
+}
+
+TEST(CNode, can_recognize_not_palindrom_by_size_right)
+{
+  struct CNode* t;
+  struct CNode* start; 
+  start = new struct CNode();
+	start->val = 0;
+	start->next = 0;
+	std::cout << start -> val << ' ';
+    t=start;
+  for(int i = 1; i < 10; i++) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+  }
+
+	for(int i = 10; i > -1; i--) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+     
+  }
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = 72;
+	t->next = 0;
+
+  std:: cout << std:: endl;
+  t=start;
+  EXPECT_NE(1, palindrom(&start));
+  while(t!=0) {
+    std::cout << t -> val << ' ';
+	t=t->next;
+  }	
+  printf("\n");
+}
+
+TEST(CNode, can_recognize_not_palindrom_by_size_left)
+{
+  struct CNode* t;
+  struct CNode* start; 
+  start = new struct CNode();
+	start->val = 72;
+	start->next = 0;
+	t=start;
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = 0;
+	t->next = 0;
+	std::cout << start -> val << ' ';
+    t=start;
+  for(int i = 1; i < 10; i++) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+  }
+
+	for(int i = 10; i > -1; i--) {
+	t->next = new struct CNode();
+	t = t->next;
+	t->val = i;
+	t->next = 0;
+	std::cout << t -> val << ' ';
+     
+  }
+
+  std:: cout << std:: endl;
+  t=start;
+  EXPECT_NE(1, palindrom(&start));
+  while(t!=0) {
+    std::cout << t -> val << ' ';
+	t=t->next;
+  }	
+  printf("\n");
 }
